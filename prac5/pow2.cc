18291fae60f054cef4e34f90de4b789350502bc6
nickhernm <jahede@gamil.com>
2024-03-06 19:24:44 +0100

    Práctica 5 creada

---
+++ prac5/pow2.cc
@@ -0,0 +1,46 @@
+//JAIME HERNÁNDEZ DELGADO 48776654W
+#include <iostream>
+using namespace std;
+
+unsigned int STEPS1 = 0;
+unsigned int STEPS2 = 0;
+unsigned int STEPS3 = 0;
+
+unsigned long pow2_1(unsigned number){
+	STEPS1++;
+	if(number == 0)
+		return 1;
+	return (2 * pow2_1(number-1));
+}
+
+unsigned long pow2_2(unsigned number){
+	STEPS2++;
+	if(number == 0)
+		return 1;
+
+	if(number%2 == 0)
+		return (pow2_2(number/2)) * (pow2_2(number/2));
+ 	return 2 * (pow2_2(number/2)) * (pow2_2(number/2));
+}
+
+unsigned long pow2_3(unsigned number){
+	STEPS3++;
+	if(number == 0)
+		return 1;
+	return pow2_3(number-1) + pow2_3(number-1);
+}
+
+int main (){
+
+	for(unsigned exp = 1; exp< 32; exp++){
+		STEPS1 = STEPS2 = STEPS3 = 0;
+		pow2_1(exp);
+		cout << STEPS1 << "\t";
+		pow2_2(exp);
+		cout << STEPS2 << "\t";
+		pow2_3(exp);
+		cout << STEPS3 << endl;
+	}
+
+	return 0;
+}
---
+++ prac5/pow2.gnuplot
@@ -0,0 +1,13 @@
+#JAIME HERNÁNDEZ DELGADO
+
+set terminal png
+set xrange [0:32]
+set yrange [0:100]
+set output "pow2.png"
+
+set xlabel "number"
+set ylabel "Program steps"
+
+plot "pow2.steps" using 1:1 with lines title "pow2(n) = 2* pow2(n-1)",\
+     "pow2.steps" using 1:2 with lines title "pow2(n) =  pow2(n/2) * pow2(n/2)",\
+     "pow2.steps" using 1:3 with lines title "pow2(n) = pow2(n-1) + pow2(n-1)",\
