602b67030c8956993c0001aef94753e81c55067a
nickhernm <jahede@gamil.com>
2024-03-16 11:48:20 +0100

    Terminar y revisar practica 5

--- prac5/pow2.cc
+++ prac5/pow2.cc
@@ -7,43 +7,52 @@ unsigned int STEPS1 = 0;
 unsigned int STEPS2 = 0;
 unsigned int STEPS3 = 0;
 
-unsigned long pow2_1(unsigned number){
-	STEPS1++;
-	if(number == 0)
-		return 1;
-	return (2 * pow2_1(number-1));
+// Coste: \Theta(n)
+unsigned long pow2_1(unsigned n) {
+    STEPS1++;
+
+    if (n == 0)
+        return 1;
+    
+    return 2 * pow2_1(n - 1);
 }
 
-unsigned long pow2_2(unsigned number){
+// Coste: \Theta(log n)
+unsigned long pow2_2(unsigned n) {
 	STEPS2++;
-	if(number == 0)
-		return 1;
 
-	if(number%2 == 0)
-		return (pow2_2(number/2)) * (pow2_2(number/2));
+    if (n == 0) return 1;
 
- 	return 2 * (pow2_2(number/2)) * (pow2_2(number/2));
+    if (n == 1) return 2;
+
+    if (n % 2 == 0)
+        return pow2_2(n / 2) * pow2_2(n / 2);
+    else
+        return pow2_2((n - 1) / 2) * pow2_2((n - 1) / 2) * 2;
+    
 }
 
-unsigned long pow2_3(unsigned number){
+// Coste: \Theta (2^n)
+unsigned long pow2_3(unsigned n){
 	STEPS3++;
-	if(number == 0)
+	if(n == 0)
 		return 1;
 	
-	return pow2_3(number-1) + pow2_3(number-1);
+	return pow2_3(n-1) + pow2_3(n-1);
 }
 
 int main (){
 
-	for(unsigned exp = 1; exp< 32; exp++){
-		STEPS1 = STEPS2 = STEPS3 = 0;
-		pow2_1(exp);
-		cout << STEPS1 << "\t";
-		pow2_2(exp);
-		cout << STEPS2 << "\t";
-		pow2_3(exp);
-		cout << STEPS3 << endl;
-	}
+	for (unsigned number = 1; number < 32; number++) {
+        STEPS1 = STEPS2 = STEPS3 = 0;
+		
+        pow2_1(number);
+        cout << number << "\t" << STEPS1 << "\t";
+        pow2_2(number);
+        cout << STEPS2 << "\t";
+        pow2_3(number);
+        cout << STEPS3 << endl;
+    }
 
 	return 0;
 }
--- prac5/pow2.gnuplot
+++ prac5/pow2.gnuplot
@@ -1,13 +1,13 @@
 #JAIME HERNÁNDEZ DELGADO  48776654W
 
 set terminal png
-set xrange [0:32]
-set yrange [0:100]
+set xrange [1:32]
+set yrange [0:50]
 set output "pow2.png"
 
-set xlabel "number"
+set xlabel "n"
 set ylabel "Program steps"
 
-plot "pow2.steps" using 1:1 with lines title "pow2(n) = 2* pow2(n-1)",\
-     "pow2.steps" using 1:2 with lines title "pow2(n) =  pow2(n/2) * pow2(n/2)",\
-     "pow2.steps" using 1:3 with lines title "pow2(n) = pow2(n-1) + pow2(n-1)",\
+plot "pow2.steps" using 1:2 with lines title "pow2_1(n) = 2 * pow2_1(n-1)", \
+     "pow2.steps" using 1:3 with lines title "pow2_2(n)", \
+     "pow2.steps" using 1:4 with lines title "pow2_3(n)"
