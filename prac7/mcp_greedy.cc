ccca864b76f9d40398d0d5e441ded98c83ccecbd
nickhernm <ramajahede@gmail.com>
2024-04-16 13:23:53 +0200

    Prac7

--- prac7/mcp_greedy.cc
+++ prac7/mcp_greedy.cc
@@ -28,8 +28,23 @@ vector<vector<int>> read_map(const string& filename) {
     return map;
 }
 
-int mcp_greedy() {
+int mcp_naive(vector<vector<int>>& mapa, int m, int n) {
+    int rows = mapa.size();
+    int cols = mapa[0].size();
 
+    if (m < 0 || n < 0 || m >= rows || n >= cols) {
+        return INT_MAX;
+    }
+
+    if (m == rows - 1 && n == cols - 1) {
+        return mapa[m][n];
+    }
+
+    int move_down = mcp_naive(mapa, m + 1, n);
+    int move_right = mcp_naive(mapa, m, n + 1);
+    int move_diagonal = mcp_naive(mapa, m + 1, n + 1);
+
+    return mapa[m][n] + min({move_down, move_right, move_diagonal});
 }
 
 int mcp_parser(vector<vector<int>>& mapa, vector<vector<char>>& path) {
@@ -77,6 +92,40 @@ int mcp_parser(vector<vector<int>>& mapa, vector<vector<char>>& path) {
     path[n-1][m-1] = 'x';
 
     return map_parser[n - 1][m - 1];
+}
+
+int mcp_greedy(const vector<vector<int>>& map) {
+    int n = map.size();
+    int m = map[0].size();
+    int cost = 0;
+
+    int i = 0, j = 0;
+    while (i < n - 1 || j < m - 1) {
+        cost += map[i][j];
+        int min_cost = map[i][j];
+        int next_i = i, next_j = j;
+
+        // Evaluar los tres movimientos posibles: derecha, abajo y diagonal
+        if (j < m - 1 && map[i][j + 1] < min_cost) {
+            min_cost = map[i][j + 1];
+            next_j = j + 1;
+        }
+        if (i < n - 1 && map[i + 1][j] < min_cost) {
+            min_cost = map[i + 1][j];
+            next_i = i + 1;
+            next_j = j;
+        }
+        if (i < n - 1 && j < m - 1 && map[i + 1][j + 1] < min_cost) {
+            min_cost = map[i + 1][j + 1];
+            next_i = i + 1;
+            next_j = j + 1;
+        }
+
+        i = next_i;
+        j = next_j;
+    }
+    cost += map[i][j];
+    return cost;
 }
 
 int main(int argc, char *argv[]) {
@@ -128,6 +177,8 @@ int main(int argc, char *argv[]) {
 
     vector<vector<char>> path(rows, vector<char>(cols, '.'));
     int parser = mcp_parser(mapa, path);
+    int greedy = mcp_greedy(mapa);
+    cout << parser << " " << greedy << endl;
 
     if (show_path_2D) {
         for (int i = 0; i < rows; ++i) {
